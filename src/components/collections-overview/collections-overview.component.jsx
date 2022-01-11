a8025d5b7016bc8e77c43da94d9574e70c469a44
Anandakumar Palanisamy <ananda11k@gmail.com>
2022-01-11 17:54:09 +0000

    correct mistake in the collection-overview component

--- src/components/collections-overview/collections-overview.component.jsx
+++ src/components/collections-overview/collections-overview.component.jsx
@@ -4,7 +4,7 @@ import { createStructuredSelector } from 'reselect';
 import { CollectionsOverviewContainer } from './collections-overview.styles';
 
 import CollectionPreview from '../collection-preview/collection-preview.component';
-import { selectCollections } from '../../redux/shop/shop.selectors';
+import { selectCollectionsForPreview } from '../../redux/shop/shop.selectors';
 
 const CollectionsOverview = ({ collections }) => (
     <CollectionsOverviewContainer>
@@ -17,7 +17,7 @@ const CollectionsOverview = ({ collections }) => (
 );
 
 const mapStateToProps = createStructuredSelector({
-    collections: selectCollections
+    collections: selectCollectionsForPreview
 });
 
 export default connect(mapStateToProps)(CollectionsOverview);
