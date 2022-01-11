18b00c12682a10b4a604a31470631cf4481b2a15
Anandakumar Palanisamy <ananda11k@gmail.com>
2022-01-11 22:34:34 +0000

    add redux-thunk and move our fetching shop collections inside the asynchronous actions using redux-thunk

--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -25,3 +25,8 @@ export const selectCollection = collectionUrlParam =>
         [selectCollections],
         collections => (collections ? collections[collectionUrlParam] : null)
     )
+
+export const selectIsCollectionFetching = createSelector(
+    [selectShop],
+    shop => shop.isFetching
+);
